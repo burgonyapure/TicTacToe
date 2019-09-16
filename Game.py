d4c08b787fa41cc720b45bea917a64d481ca008a
Its707notLOL <54311015+Its707notLOL@users.noreply.github.com>
2019-09-16 13:09:11 +0200

    Add files via upload

---
+++ Game.py
@@ -0,0 +1,29 @@
+# Lekezelni a user hibait
+
+# Mukodjon
+
+# Egyszeru
+
+# alap jatek szabaly
+
+# .txt file ---> update on every turn 
+
+game = [[0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]]
+
+print(" 0 1 2")
+print(game)
+
+
+
+
+
+
+
+
+
+
+
+
+
