65a1de0c70a765138b4e2dd1bbb4973c688b52cf
Its707notLOL <54311015+Its707notLOL@users.noreply.github.com>
2019-09-16 14:15:42 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -11,6 +11,11 @@ game = ["-", "-", "-",
         "-", "-", "-",
         "-", "-", "-"]
 
+
+
+
+
+
 def display_game_board():
         print(game[0] + " | " + game[1] + " | " + game[2])    
         print(game[3] + " | " + game[4] + " | " + game[5])    
@@ -22,9 +27,10 @@ def check_if_game_over():
         return False
         
 
+# Kezd egy uj jatekot
 
-
-
+def play_game():
+        display_game_board()
 
 
 
@@ -38,52 +44,56 @@ def check_if_game_over():
 # WINNING script
 def X_Winner():
         if  game[0] == "X" and game[1] == "X" and game[2] == "X":
-                print("The winner is {}".format(X_Player))
         # Player1 = X_Player ,  hogyha player1 "X"-el kezd.
-                check_if_game_over = True
+                check_if_game_over == True
 
         elif  game[3] == "X" and game[4] == "X" and game[5] == "X":
-                print("The winner is {}".format(X_Player))
-                check_if_game_over = True
+                check_if_game_over == True
 
         elif  game[6] == "X" and game[7] == "X" and game[8] == "X":
-                print("The winner is {}".format(X_Player))
-                check_if_game_over = True
+                check_if_game_over == True
 
         elif  game[0] == "X" and game[4] == "X" and game[8] == "X":
-                print("The winner is {}".format(X_Player))
-                check_if_game_over = True
+                check_if_game_over == True
 
         elif  game[6] == "X" and game[4] == "X" and game[2] == "X":
-                print("The winner is {}".format(X_Player))
-                check_if_game_over = True
+                check_if_game_over == True
 
-
+# global variable == Winner
 
 
 def O_Winner():
         if  game[0] == "O" and game[1] == "O" and game[2] == "O":
-                print("The winner is {}".format(O_Player))
-                check_if_game_over = True
-
+                check_if_game_over == True
+                is_O_the_winner == True
         elif  game[3] == "O" and game[4] == "O" and game[5] == "O":
-                print("The winner is {}".format(O_Player))
-                check_if_game_over = True
+                check_if_game_over == True
+                is_O_the_winner == True
 
         elif  game[6] == "O" and game[7] == "O" and game[8] == "O":
-                print("The winner is {}".format(O_Player))
-                check_if_game_over = True
+                check_if_game_over == True
+                is_O_the_winner == True
 
         elif  game[0] == "O" and game[4] == "O" and game[8] == "O":
-                print("The winner is {}".format(O_Player))
-                check_if_game_over = True
+                check_if_game_over == True
+                is_O_the_winner == True
 
         elif  game[6] == "O" and game[4] == "O" and game[2] == "O":
-                print("The winner is {}".format(O_Player))
-                check_if_game_over = True
+                is_O_the_winner == True
+                
+                check_if_game_over == True
+        
 
-# game_still_going MEGNEZNI megy e meg a jatek
+# game_still_going MEGNEZNI megy-e meg a jatek
         game_still_going = False
+
+def is_O_the_winner():
+        if True:
+                print("The winner is {}".format(O_Player))
+
+def is_X_the_winner():
+        if True:
+                print("The winner is {}".format(X_Player))
 
 
 
