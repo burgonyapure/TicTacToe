e239b2f347c526a8a75ad57cc484d7429665fc9a
Its707notLOL <54311015+Its707notLOL@users.noreply.github.com>
2019-09-16 13:25:15 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -7,18 +7,15 @@
 # alap jatek szabaly
 
 # .txt file ---> update on every turn 
+game = ["-", "-", "-",
+        "-", "-", "-",
+        "-", "-", "-"]
 
-game = [[0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]]
-
-def game_board():
-        print("   A  B  C")
-        for count, row in enumerate(game):
-                print(count, row)
-                
-game_board()                
-
+def display_game_board():
+        print(game[0] + " | " + game[1] + " | " + game[2])    
+        print(game[3] + " | " + game[4] + " | " + game[5])    
+        print(game[6] + " | " + game[7] + " | " + game[8])           
+display_game_board() 
 
 
 
